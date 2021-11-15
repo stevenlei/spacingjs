4db5af5377b040b4a7df9ae9f5c1c4e374ea8f6d
Steven Lei <stevenplus@gmail.com>
2021-11-16 00:37:29 +0800

    update build and bundle

--- extensions/tampermonkey/spacingjs_1.0.7.js
+++ extensions/tampermonkey/spacingjs_1.0.7.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Spacing JS
+// @version      1.0.6
+// @description  Measure the spacing between elements on webpage. Hold down Alt (Windows) / Option (Mac) key, and move the cursor.
+// @author       contact@stevenlei.com
+// @namespace    stevenlei
+// @grant        none
+// @run-at       document-start
+// @include      *://*
+// ==/UserScript==
+
 /*!
  * Spacing.js v1.0.7
  * Copyright (c) 2021 Steven Lei
