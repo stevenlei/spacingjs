c6a3e59748e01de624fa41e4899ab0bf13b74464
stevenlei <contact@stevenlei.com>
2024-12-21 01:07:40 +0800

    improve build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,74 @@ const webpack = require("webpack");
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const { version } = require("./package.json");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const banner = `/*!
+ * Spacing.js v${version}
+ * Copyright (c) 2021-${new Date().getFullYear()} Steven Lei
+ * Released under the MIT License.
+ */`;
 
 /**
- * @type {webpack.Configuration}
+ * @type {(env: any, argv: { mode: string }) => webpack.Configuration}
  */
-module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    library: {
-      name: "Spacing",
-      type: "window",
-    },
-  },
-  resolve: {
-    extensions: [".ts", ".js", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)s$/,
-        loader: "babel-loader",
-        exclude: /(node_modules)/,
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const filename = isProduction ? 'spacing.min.js' : 'spacing.js';
+
+  return {
+    entry: "./src/index.ts",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename,
+      library: {
+        name: "Spacing",
+        type: "umd",
+        export: "default",
       },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            pure_funcs: ["console.info", "console.debug", "console.log"],
-          },
+      globalObject: 'this',
+      clean: true,
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    resolve: {
+      extensions: [".ts", ".js", ".json"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(t|j)s$/,
+          loader: "babel-loader",
+          exclude: /(node_modules)/,
         },
-        extractComments: false,
+      ],
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            format: {
+              comments: /@license|@preserve|^!/i,
+            },
+            compress: {
+              pure_funcs: ["console.info", "console.debug", "console.log"],
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner,
+        raw: true,
+        entryOnly: true,
       }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
+        'process.env.VERSION': JSON.stringify(version),
+      }),
+      ...(isProduction && env.analyze ? [new BundleAnalyzerPlugin()] : []),
     ],
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: `/*!\n * Spacing.js v${version}\n * Copyright (c) 2021 Steven Lei\n * Released under the MIT License.\n*/`,
-      raw: true,
-    }),
-  ],
+  };
 };
